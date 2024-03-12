b1ddde7f42e3f1c20f78fe8738bc2af2e4c9fbda
Amisadai Lechuga <amisadailechu@gmail.com>
2024-03-12 09:13:10 -0600

    12: Condicional Ternario

---
+++ src/bases/12-ternario.js
@@ -0,0 +1,35 @@
+
+////------Condiciones Normales Ejemplo 1----------
+//const activo = true;
+
+//let mensaje = '';
+
+//if ( activo ) {
+//    mensaje = 'Activo';
+//} else {
+//    mensaje = 'Inactivo';
+//}
+
+//console.log(mensaje);
+
+
+////-------------------------------------------------
+
+
+
+//---------condicional Ternario ejemplo 2-------------
+
+//const activo = true ;
+//const mensaje = ( activo ) ? 'Activo' : 'Inactivo';
+
+//console.log(mensaje);
+////----------------------------------------------------
+
+
+//condicionale Ternario otra forma ejemplo 3
+
+const activo = true;
+
+const mensaje = activo && 'Activo';
+
+console.log (mensaje);
